f81e2071f4fb1f6d44fd336a765b93a565bee03c
n_cho <n_cho@yumemi.co.jp>
2013-06-25 16:54:36 +0900

    add all sdk

---
+++ SNSBigBang/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SNSBigBang
+//
+//  Created by 張 寧 on 2013/06/25.
+//  Copyright (c) 2013年 張 寧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SNSBigBang/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SNSBigBang
+//
+//  Created by 張 寧 on 2013/06/25.
+//  Copyright (c) 2013年 張 寧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
