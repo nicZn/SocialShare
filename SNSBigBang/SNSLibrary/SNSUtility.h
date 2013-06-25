b72ae4d286764b34d069971283ab21c6474f88e9
n_cho <n_cho@yumemi.co.jp>
2013-06-25 18:12:18 +0900

    add sns config

---
+++ SNSBigBang/SNSLibrary/SNSUtility.h
@@ -0,0 +1,29 @@
+//
+//  SNSUtility.h
+//  SNSBigBang
+//
+//  Created by 張 寧 on 2013/06/25.
+//  Copyright (c) 2013年 張 寧. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//Weixin config
+#define WeixinAppId  @"wxd25b2dd6207a36f7"
+#define WeixinAppKey @"f36df7f35cc9385a9b260b45210898f4"
+
+
+//Renren config
+#define RenrenAppId     @"237526"
+#define RenrenAppKey    @"7627857571a64196bdf72f5e33762869"
+#define RenrenSecertKey @"731a2b281dcd4e8fa5148dd9b7e00755"
+
+
+//Weibo config
+
+#define WeiboAppKey    @"3094941605"
+#define WeiboSecertKey @"1ae5122b9dfe1c102aae56cc33f98b67"
+
+@interface SNSUtility : NSObject
+
+@end
---
+++ SNSBigBang/SNSLibrary/SNSUtility.m
@@ -0,0 +1,14 @@
+//
+//  SNSUtility.m
+//  SNSBigBang
+//
+//  Created by 張 寧 on 2013/06/25.
+//  Copyright (c) 2013年 張 寧. All rights reserved.
+//
+
+#import "SNSUtility.h"
+
+
+@implementation SNSUtility
+
+@end
